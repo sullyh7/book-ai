ba54ff3b0778cecbf502e688e42e7af5fee93396
sulayman <ec22675@qmul.ac.uk>
2023-08-31 22:12:49 +0100

    added google login fixed

--- providers/Provider.tsx
+++ providers/Provider.tsx
@@ -3,12 +3,8 @@
 import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
-interface ProviderProps {
-    children: React.ReactNode,
-    session: Session,
-}
 
-const Provider = ({ children, session }: ProviderProps) => (
+const Provider = ({ children, session }: any) => (
   <SessionProvider session={session}>
     {children}
   </SessionProvider>
